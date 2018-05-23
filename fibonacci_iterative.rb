c5eee94836222af987d16046a4b4107335e0fc11
Quentin <quent.guillaume@gmail.com>
2018-05-23 14:13:13 -0700

    first commit

---
+++ fibonacci_iterative.rb
@@ -0,0 +1,34 @@
+def fib(n)
+fib = []
+  return 0 if n == 0
+  return 1 if n == 1
+
+  fib_0 = 0
+  fib_1 = 1
+
+(1.. n).each do
+  temp = fib_0
+  fib_0 = fib_1
+
+  fib_1 += temp
+  fib << fib_0
+
+  if fib.length == 10
+   break
+ end
+
+ end
+ print fib
+end
+
+
+puts fib(0)
+puts fib(1)
+puts fib(2)
+puts fib(3)
+puts fib(4)
+puts fib(5)
+puts fib(6)
+puts fib(7)
+puts fib(8)
+puts fib(9)
